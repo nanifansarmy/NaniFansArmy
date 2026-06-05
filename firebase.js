014a9afbd2b3c79244d36af3b5d76c325d569229
nanifansarmy <nanifansarmy@gmail.com>
2026-06-05 18:39:11 +0530

    Analytics Code Added for firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
     import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+    import { getAnalytics } from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
 import {
   getFirestore,
   doc,
@@ -21,6 +23,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 console.log("Firebase Connected Successfully");
